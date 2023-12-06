a0e53b8e8cfdfdd84a8ab474968f2e295f89f087
Stephen Regan James Bacolod Layson <jameslayson.0@gmail.com>
2023-12-07 02:13:17 +0800

    update

--- src/app/login/authorized/[department]/appointments/schedule/page.jsx
+++ src/app/login/authorized/[department]/appointments/schedule/page.jsx
@@ -15,6 +15,8 @@ const Schedule = ({ params }) => {
 	const [filter,setFilter] = useState("");
 	const [SelectedDay, setSelectedDay] = useState(null);
 
+	const [hasSchedule, setHasSchedule] = useState(null);
+
 	const formatDate = (timestamp) => {
 		const options = { month: 'short', day: 'numeric', year: 'numeric' };
 		const formattedDate = new Date(timestamp).toLocaleDateString(undefined, options);
@@ -71,21 +73,26 @@ const Schedule = ({ params }) => {
 			<div className={styles.TimeSelection}>
 				<p style={{ fontWeight: 'bold', textAlign: 'center' }}>Availability</p>
 
-				<label className={styles.radioForm} htmlFor="schedulingtime1"><input type="radio" name="test" id="schedulingtime1" value="8am-10am" />8am-10am</label>
+				{hasSchedule ? null :
+					<>
+						<label className={styles.radioForm} htmlFor="schedulingtime1"><input type="radio" name="test" id="schedulingtime1" value="8am-10am" />8am-10am</label>
 
-				<label className={styles.radioForm} htmlFor="schedulingtime2"><input type="radio" name="test" id="schedulingtime2" value="10am-12pm" />10am-12pm</label>
+						<label className={styles.radioForm} htmlFor="schedulingtime2"><input type="radio" name="test" id="schedulingtime2" value="10am-12pm" />10am-12pm</label>
 
-				<label className={styles.radioForm} htmlFor="schedulingtime3"><input type="radio" name="test" id="schedulingtime3" value="1pm-3pm" />1pm-3pm</label>
+						<label className={styles.radioForm} htmlFor="schedulingtime3"><input type="radio" name="test" id="schedulingtime3" value="1pm-3pm" />1pm-3pm</label>
 
-				<label className={styles.radioForm} htmlFor="schedulingtime4"><input type="radio" name="test" id="schedulingtime4" value="3pm-5pm" />3pm-5pm</label>
+						<label className={styles.radioForm} htmlFor="schedulingtime4"><input type="radio" name="test" id="schedulingtime4" value="3pm-5pm" />3pm-5pm</label>
 
-				<label className={styles.radioForm} htmlFor="schedulingtime5"><input type="radio" name="test" id="schedulingtime5" value="morning" />Morning</label>
+						<label className={styles.radioForm} htmlFor="schedulingtime5"><input type="radio" name="test" id="schedulingtime5" value="morning" />Morning</label>
 
-				<label className={styles.radioForm} htmlFor="schedulingtime6"><input type="radio" name="test" id="schedulingtime6" value="afternoon" />Afternoon</label>
+						<label className={styles.radioForm} htmlFor="schedulingtime6"><input type="radio" name="test" id="schedulingtime6" value="afternoon" />Afternoon</label>
 
-				<label className={styles.radioForm} htmlFor="schedulingtime7"><input type="radio" name="test" id="schedulingtime7" value="wholeday" />Whole Day</label>
+						<label className={styles.radioForm} htmlFor="schedulingtime7"><input type="radio" name="test" id="schedulingtime7" value="wholeday" />Whole Day</label>
 
-				<button>Add</button>
+						<button onClick={OnAdd}>Add</button>
+					</>
+				}
+
 			</div>
 
 			<div className={styles.AppoitmentDetails}>
