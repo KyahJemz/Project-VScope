86c6c187c4edb92eb759cdd196ffea153378c511
Saantillan <155517616+Saantillan@users.noreply.github.com>
2024-01-03 14:49:07 +0800

    update

--- src/app/about/data.js
+++ src/app/about/data.js
@@ -2,36 +2,36 @@ export const data = {
        aboutvscope: [
               {
                      title: "About V-Scope",
-                     desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-                     image: "qeqweqwewqeqw",
+                     desc: "VScope is formed by merging the words vital and scope, which implies the significance of well-being and the wide array of data encompassed by the system. VScope is strategically positioned to enhance and refine the existing infrastructure of Medical, Dental, and Student Development and Placement Center (SDPC) services.",
+                     image: "https://i.imgur.com/4immlsF.png",
               },
        ],
        developers: [
               {
                      id: 1,
-                     name: "Developer 1", 
-                     image: "",
+                     name: "Dan Vic B. Chua", 
+                     image: "https://i.imgur.com/gLY8idJ.jpg",
               },
               {
                      id: 2,
-                     name: "Developer 2", 
-                     image: "",
+                     name: "Leonard Directo", 
+                     image: "https://i.imgur.com/iApsO5e.png",
               },
               {
                      id: 3,
-                     name: "Developer 3", 
-                     image: "",
+                     name: "Elhaiza Barbonio", 
+                     image: "https://i.imgur.com/zc3b0rn.png",
               },
               {
                      id: 4,
-                     name: "Developer 4", 
-                     image: "",
+                     name: "Alyanna Escasinas", 
+                     image: "https://i.imgur.com/B7OcniV.png",
               },
          
               {
                      id: 5,
-                     name: "Developer 5", 
-                     image: "",
+                     name: "Lucky Jude Santillan", 
+                     image: "https://i.imgur.com/KPTTUrf.png",
               },
          
          
@@ -40,26 +40,26 @@ export const data = {
               {
                      id: 1,
                      title: "About SSCR - de Cavite",
-                     desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-                     image: "",
+                     desc: "Sebastian College-Recoletos de Cavite is resolute to face the challenges brought upon its educational apostolate by providing its students with the most effective ways in dealing with challenges of education in a pandemic era by adapting the Recoletos Hybrid-Flexible (RHy-Flex) Learning Program. This learning modality fuses the in-person and online experiences-contextualizing learning by providing hands-on activities enhanced by technology.",
+                     image: "https://i.imgur.com/NEAyaf3.png",
               },
               {
                      id: 2,
                      title: "About Medical Health Services",
                      desc: "The main objective of our medical clinic is to maintain and/or restore health of all members of the Sebastinian community specially the students. It also aims to impart health education and disseminate information relative to the promotion and preservation of health and prevention and/or cure of illnesses.",
-                     image: "",
+                     image: "https://i.imgur.com/4dEzbKy.png",
               },
               {
                      id: 3,
                      title: "About Dental Health Services",
                      desc: "The school dentist shall provide preventive as well as curative services to all bona fide students. However, should there be cases not within the scope of the services provided by the clinic, the School Dentist shall have the responsibility to make the necessary referrals.",
-                     image: "",
+                     image: "https://i.imgur.com/4dEzbKy.png",
               },
               {
                      id: 4,
                      title: "About SDPC",
                      desc: "The Student Development and Placement Center of San Sebastian College – Recoletos de Cavite works as an integral part of the total educational process. It primarily provides for the holistic development of the students to the fullest actualization of their potential while extending its services to families, alumni, and members of the community.",
-                     image: "",
+                     image: "https://i.imgur.com/FWAH6wd.png",
               },
        ],
        images: [
@@ -76,5 +76,3 @@ export const data = {
                      image: "",
               },
        ]
-};
-     
