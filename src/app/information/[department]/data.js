c048b6ea94d088941a991c527082c5e08b06eda9
Saantillan <155517616+Saantillan@users.noreply.github.com>
2024-01-09 15:38:24 +0800

    update

--- src/app/information/[department]/data.js
+++ src/app/information/[department]/data.js
@@ -2,23 +2,23 @@ export const items = {
   SDPC: [
     {
       id: 1,
-      title: "Services 1",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+      title: "Counseling Services",
+      desc: "This service provides a helping relationship between the counselor and the client. If the students and/or members of the community feel the need to talk to a counselor, they can come at the SDPC office anytime.",
+      image: "https://i.imgur.com/L74lT5E.png",
       button_1: {text: "Back", url: "/services"},
       
     },
     {
       id: 2,
-      title: "Services 2",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+      title: "Referral",
+      desc: "This service offers assistance to the clients in obtaining services from other people or agencies that might be more effective in helping them with their special needs. Normal-ly, referrals are made to other individuals, particularly specialists, who might be in a better position in handling the case and responding to the peculiar needs of the student or the client.",
+      image: "https://i.imgur.com/1cCBEwW.jpg",
       button_1: {text: "Back", url: "/services"},
     },
     {
       id: 3,
-      title: "Services 3",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      title: "Psychological Testing and Assessment Service",
+      desc: "This service provides assessment of the intellectual ability, interest, potential, personality and achievement level of students. It seeks to identify students’ individualism that will contribute to a deeper awareness and understanding of oneself.",
       image:"https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
       button_1: {text: "Back", url: "/information"},
     }
@@ -26,23 +26,23 @@ export const items = {
   Medical: [
     {
       id: 1,
-      title: "Services 1",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image: "https://i.imgur.com/reIgCuR.jpg",
+      title: "Medications",
+      desc: "The giving of medications for symptomic relief of minor ailments such as cough, colds, headache, muscle pains, etc.",
+      image: "https://i.imgur.com/ZzSjv1V.jpg",
       button_1: {text: "Back", url: "/information"},
     },
     {
       id: 2,
-      title: "Services 2",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image: "https://i.imgur.com/rq7cvgn.jpg",
+      title: "Medical Certificates",
+      desc: "Issuance of medical certificates. A medical certificate shall be issued only after the concerned student, faculty member, or non-academic personnel has been examined and treated by the school physician.",
+      image: "https://i.imgur.com/SSIFzXd.jpg",
       button_1: {text: "Back", url: "/information"},
     },
     {
       id: 3,
-      title: "Services 3",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image: "https://i.imgur.com/VkvYJia.jpg",
+      title: "Blood pressure check-ups",
+      desc: "Blood pressure check-ups for hypertensive patients or whoever shall need or request.",
+      image: "https://i.imgur.com/7ur3cda.jpg",
       button_1: {text: "Back", url: "/information"},
     }
   ],
@@ -51,7 +51,7 @@ export const items = {
       id: 1,
       title: "Routine Dental Examination",
       desc: "Conducting routine dental examinations for all students during enrollment is a proactive approach to promote oral health and ensure that students' dental needs are addressed.",
-      image: "https://i.imgur.com/BKJs6CZ.png",
+      image: "https://i.imgur.com/doxguY4.jpg",
       button_1: {text: "Back", url: "/information"},
     },
     {
@@ -59,14 +59,14 @@ export const items = {
       title: "Dental Consultation",
       desc: "The rendering of individual dental consultation or treatment which shall include the following but shall not include tooth extraction:<br/><br/><ol><li>Oral examination and / or chair side consultation</li><li>Oral prophylaxis</li><li>First-aid treatment of traumatic injuries suffered while inside the school campus</li><li>Control of secondary or post-traumatic hemorrhage</li></ol>"
       ,
-      image: "https://i.imgur.com/GfPFTAQ.jpeg",
+      image: "https://i.imgur.com/w00c0WD.jpg",
       button_1: {text: "Back", url: "/information"},
     },
     {
       id: 3,
       title: "Referral to Dental Specialists",
       desc: "The referral to dental specialists like orthodontists, periodontists, and oral surgeons of patients for further treatment and management depending upon the presented dental cases.",
-      image: "https://i.imgur.com/kuRCMh0.jpg",
+      image: "https://i.imgur.com/GCdBAHo.jpg",
       button_1: {text: "Back", url: "/information/services"},
     }
   ],
